c0a12f00cbb12608bb780a0ed84b338be783143d
Sam <samcope24@gmail.com>
2022-11-26 17:25:24 +0000

    add comments to shell script

--- Api/Scripts/Dockerize.sh
+++ Api/Scripts/Dockerize.sh
@@ -2,18 +2,22 @@
 set -e
 cd ..
 
+# check to see if container is running
 if [[ $(docker ps -q -f "name=api-container") ]]; then
     docker stop api-container
 fi
 
+# check to see if container needs to be removed
 if [[ $(docker ps -a -q -f "name=api-container") ]]; then
     docker rm api-container
 fi
 
+# check to see if image needs to be removed
 if [[ $(docker images -q api-image) ]]; then
     docker image rm api-image
 fi
 
+# build the new container
 docker build -t api-image -f Dockerfile .
 docker create --name api-container api-image
 docker start api-container
